f3356e63bb52dbdeb58cc02afe63e450cb6a7235
Quentin Ikeno <ikenoq@gmail.com>
2022-06-06 15:23:08 -1000

    Add tests for GET /jobs

--- routes/jobs.test.js
+++ routes/jobs.test.js
@@ -56,3 +56,111 @@ describe("POST /jobs", () => {
 		expect(resp.statusCode).toBe(400);
 	});
 });
+
+/************************************** GET /jobs */
+
+describe("GET /jobs", () => {
+	test("Returns a list of jobs", async () => {
+		const resp = await request(app).get("/jobs");
+		expect(resp.statusCode).toBe(200);
+		expect(resp.body).toEqual({
+			jobs: [
+				{
+					id: expect.any(Number),
+					title: "Job 1",
+					salary: 50000,
+					equity: "0",
+					companyHandle: "c1",
+				},
+				{
+					id: expect.any(Number),
+					title: "Job 2",
+					salary: 100000,
+					equity: "1",
+					companyHandle: "c2",
+				},
+				{
+					id: expect.any(Number),
+					title: "Sound Engineer",
+					salary: 50000,
+					equity: "0",
+					companyHandle: "c3",
+				},
+			],
+		});
+	});
+
+	test("Works with title filter", async () => {
+		const resp = await request(app).get("/jobs").query({ title: "job" });
+		expect(resp.statusCode).toBe(200);
+		expect(resp.body).toEqual({
+			jobs: [
+				{
+					id: expect.any(Number),
+					title: "Job 1",
+					salary: 50000,
+					equity: "0",
+					companyHandle: "c1",
+				},
+				{
+					id: expect.any(Number),
+					title: "Job 2",
+					salary: 100000,
+					equity: "1",
+					companyHandle: "c2",
+				},
+			],
+		});
+	});
+
+	test("Works with minSalary filter", async () => {
+		const resp = await request(app)
+			.get("/jobs")
+			.query({ minSalary: 70000 });
+		expect(resp.statusCode).toBe(200);
+		expect(resp.body).toEqual({
+			jobs: [
+				{
+					id: expect.any(Number),
+					title: "Job 2",
+					salary: 100000,
+					equity: "1",
+					companyHandle: "c2",
+				},
+			],
+		});
+	});
+	test("Works with hasEquity filter", async () => {
+		const resp = await request(app).get("/jobs").query({ hasEquity: true });
+		expect(resp.statusCode).toBe(200);
+		expect(resp.body).toEqual({
+			jobs: [
+				{
+					id: expect.any(Number),
+					title: "Job 2",
+					salary: 100000,
+					equity: "1",
+					companyHandle: "c2",
+				},
+			],
+		});
+	});
+
+	test("Works with all three filters", async () => {
+		const resp = await request(app)
+			.get("/jobs")
+			.query({ title: "job", minSalary: 70000, hasEquity: true });
+		expect(resp.statusCode).toBe(200);
+		expect(resp.body).toEqual({
+			jobs: [
+				{
+					id: expect.any(Number),
+					title: "Job 2",
+					salary: 100000,
+					equity: "1",
+					companyHandle: "c2",
+				},
+			],
+		});
+	});
+});
